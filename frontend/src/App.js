d9a49fe07fdcc4137c941dc00a98b2644d52f8ff
Kirubakarraj Y <kirubay.05@gmail.com>
2024-09-14 16:49:27 +0530

    initial header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Images Gallery</h1>
+      <Header title="Images Gallery"/>
     </div>
   );
 }
