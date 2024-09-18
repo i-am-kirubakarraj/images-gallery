a1f60ba4fc6487704cc538391aacfc40ad68c009
Kirubakarraj Y <kirubay.05@gmail.com>
2024-09-18 17:04:00 +0530

    Controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
+import Search from './components/search';
+
 
 function App() {
+  const [word,setWord] = useState('');
+  
+  const handleSearchSubmit=(e) =>{
+    e.preventDefault();
+    console.log(word);
+  }
+  
   return (
     <div className="App">
       <Header title="Images Gallery"/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
-
+ 
 export default App;
