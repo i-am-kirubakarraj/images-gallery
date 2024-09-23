691c9d3bd586d9210d7ca75e4e16ce67cfe080f4
Kirubakarraj Y <kirubay.05@gmail.com>
2024-09-23 16:52:11 +0530

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
 import Search from './components/search';
 
+const UNSPLASH_KEY=process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word,setWord] = useState('');
@@ -11,8 +12,16 @@ function App() {
   const handleSearchSubmit=(e) =>{
     e.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
-  
+
   return (
     <div className="App">
       <Header title="Images Gallery"/>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Updated import
 import './css/index.css';
 import App from './App';
 
-ReactDOM.render(
+// Create a root and render the App component
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create root
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
