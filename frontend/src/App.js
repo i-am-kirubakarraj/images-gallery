0b6f85c0016f764bd9d05d5d9d413605a7e11852
Kirubakarraj Y <kirubay.05@gmail.com>
2024-09-25 00:10:52 +0530

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,51 @@
-import { useState } from 'react';
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/header';
-import Search from './components/search';
+import { useState } from "react";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/header";
+import Search from "./components/search";
+import ImageCard from "./components/ImageCard";
+import { Container, Row, Col } from "react-bootstrap";
 
-const UNSPLASH_KEY=process.env.REACT_APP_UNSPLASH_KEY;
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  const [word,setWord] = useState('');
-  const [images,setImages] = useState([]);
-  
-  console.log(images);
+  const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
 
-  const handleSearchSubmit=(e) =>{
+  const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => {
-        setImages([data, ...images]);
+        setImages([{ ...data, title: word }, ...images]);
       })
       .catch((err) => {
         console.log(err);
-      })
-    setWord('');
-  }
+      });
+    setWord("");
+  };
+
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id != id ));
+  };
 
   return (
     <div className="App">
-      <Header title="Images Gallery"/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} deleteImage={handleDeleteImage}/>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
- 
+
 export default App;
