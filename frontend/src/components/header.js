b5b642b5acf79241ca1d93c2af77a49a8b80388b
Kirubakarraj Y <kirubay.05@gmail.com>
2024-09-25 00:36:29 +0530

    edit text logo

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Navbar, Container } from 'react-bootstrap'
+import { ReactComponent as Logo } from '../images/logo.svg';
 
 const NavbarStyle = {
-    backgroundColor: 'lightblue'
+    backgroundColor: '#E6E6FA'
 };
 
 const Header = ({title}) => {
     return (
         <Navbar style= {NavbarStyle} variant="light">
             <Container>
-                <Navbar.Brand href="/">{title}</Navbar.Brand>
+                <Logo style={{ maxWidth: '12rem', maxHeight: '2rem' }}/>
             </Container>
         </Navbar>
     )
