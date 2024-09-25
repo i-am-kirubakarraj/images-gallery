65f3887d4a0f2d695baaad6bf0aca9012061a3f6
Kirubakarraj Y <kirubay.05@gmail.com>
2024-09-25 19:16:33 +0530

    edited button variant in welcome component

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -10,7 +10,7 @@ const Welcome = () => {
             order to start enter any search term in the input field.
             </p>
             <p>
-            <Button bsStyle="primary" href="https://unsplash.com" target="_blank">
+            <Button variant="primary" href="https://unsplash.com" target="_blank">
                 Learn more
             </Button>
             </p>
